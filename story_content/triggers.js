0046c6e60b7e4228adf2cccd9349bac25d5e926a
Angela VonSchmittou <avonschmittou@gmail.com>
2025-08-05 14:22:48 -0500

    Update 6

--- story_content/triggers.js
+++ story_content/triggers.js
@@ -2,6 +2,9 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6nVdodd5QKr":
+        Script1();
+        break;
   }
 }
 
@@ -20,4 +23,21 @@ var showPointer = player.showPointer;
 var hidePointer = player.hidePointer;
 var slideWidth = player.slideWidth;
 var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  const target = object('65xwk0DqAgn');
+const duration = 750;
+const easing = 'ease-out';
+const id = '60EjpktGNFf';
+const bounceAmount = 1;
+player.addForTriggers(
+id,
+target.animate(
+player.emphasis.bounce(bounceAmount)
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
 };
